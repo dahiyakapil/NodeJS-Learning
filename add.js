b5173f9adeee365ff69218f03fe9afef398d852e
KAPIL DAHIYA <kapildahiya309@gmail.com>
2025-01-17 14:28:10 +0530

    feat: add examples and insights on Node.js modules (ESM and CommonJS)
    - Demonstrated ESM with import/export syntax using `add.js`.
    - Included examples of CommonJS with `require` and `module.exports`.

---
+++ add.js
@@ -0,0 +1,12 @@
+console.log("Hey from add.js file");
+
+// export function sum(a, b){
+//     return a + b;
+// }
+
+function sum(a, b) {
+  return a + b;
+}
+
+// using commonjs module we are exporting the file
+module.exports = {sum}
--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+// import {sum} from "./add.js"
+
+const{ sum }= require("./add")
+
+console.log(sum(1, 2));
+
 console.log("Hello from Node.js");
 let a = 10;
 let b = 20;
@@ -9,3 +15,9 @@ console.log(this) // returns undefined : when we use ESM(ECMA Script Module) typ
 console.log(globalThis) // this will refers to the global object
 
 console.log(global === globalThis) // true
+
+// x = 45; // eferenceError: x is not defined     as we are exexcuting the code in ES module if we execute the same code in commonjs module this not throw an error. Because commonjs runs the code in non-strict mode. and the global varibal will attact to the global object
+// console.log(x)
+
+
+console.log(module.exports) // returns empty object
