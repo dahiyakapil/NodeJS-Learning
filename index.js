279588abad0d052a37f4d5e71d33c9130c2c2ec6
KAPIL DAHIYA <kapildahiya309@gmail.com>
2025-01-17 12:35:22 +0530

    Learned about the global object Node  -> global, globalThis, this Browser -> window, this, self, frames, globalThis

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log("Hello from Node.js");
+let a = 10;
+let b = 20;
+console.log(a + b);
+
+// Global object in node... different from the Browser window
+console.log(global)
+console.log(this) // returns undefined : when we use ESM(ECMA Script Module) type: "module" , and when we use the commonjs then it returns the empty object {}
+console.log(globalThis) // this will refers to the global object
+
+console.log(global === globalThis) // true
